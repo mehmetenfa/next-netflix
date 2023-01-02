4572bb1d02c75c12e25762fd11d2cb9e4ecafd6e
qxan <mehmet.enfa@gmail.com>
2023-01-02 15:35:21 +0300

    typings

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,14 @@
 import Head from "next/head";
-import type { NextPage } from "next";
 import Header from "../components/Header";
 import Banner from "../components/Banner";
+import requests from "../utils/requests";
+import { Movie } from "../typings";
 
+interface Props {
+  netflixOriginals: Movie
+}
 
-const Home: NextPage = () => {
+const Home = ({ netflixOriginals}: Props) => {
   return (
     <div className="related h-screen bg-gradient-to-b from-gray-900/10 to-[#010511] lg:h-[140vh]">
       <Head>
@@ -33,3 +37,38 @@ const Home: NextPage = () => {
 };
 
 export default Home;
+
+export const getServerSideProps = async () => {
+  const [
+    netflixOriginals,
+    trendingNow,
+    topRated,
+    actionMovies,
+    comedyMovies,
+    horrorMovies,
+    romanceMovies,
+    documentaries,
+  ] = await Promise.all([
+    fetch(requests.fetchNetflixOriginals).then((res) => res.json()),
+    fetch(requests.fetchTrending).then((res) => res.json()),
+    fetch(requests.fetchTopRated).then((res) => res.json()),
+    fetch(requests.fetchActionMovies).then((res) => res.json()),
+    fetch(requests.fetchComedyMovies).then((res) => res.json()),
+    fetch(requests.fetchHorrorMovies).then((res) => res.json()),
+    fetch(requests.fetchRomanceMovies).then((res) => res.json()),
+    fetch(requests.fetchDocumentaries).then((res) => res.json()),
+  ]);
+
+  return {
+    props: {
+      netflixOriginals: netflixOriginals.results,
+      trendingNow: trendingNow.results,
+      topRated: topRated.results,
+      actionMovies: actionMovies.results,
+      comedyMovies: comedyMovies.results,
+      horrorMovies: horrorMovies.results,
+      romanceMovies: romanceMovies.results,
+      documentaries: documentaries.results,
+    },
+  }
+}
