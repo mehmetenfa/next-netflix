aee482f9e6dee532ddae1d456bfb2ce127406f98
qxan <mehmet.enfa@gmail.com>
2023-01-03 16:25:40 +0300

    usestate banner

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -1,12 +1,20 @@
-import React from 'react'
 import Image from 'next/image'
+import { Movie } from '../typings'
+import { useState } from 'react'
 
-const Banner = () => {
+interface Props {
+  netflixOriginals: Movie[]
+}
+
+function Banner({ netflixOriginals }: Props) {
+
+  const [movie, setMovie] = useState<Movie | null>(null)
+
   return (
     <div>
-        {/* <div>
-            <Image/>
-        </div> */}
+        <div>
+            {/* <Image/> */}
+        </div>
     </div>
   )
 }
