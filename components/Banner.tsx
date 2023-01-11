b7b8d5974b93299c96bddd9b85b7908c4a810361
qxan <mehmet.enfa@gmail.com>
2023-01-11 21:05:42 +0300

    banner

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -1,28 +1,36 @@
 import Image from 'next/image'
-import { Movie } from '../typings'
-import { useState, useEffect } from 'react'
+import { useState, useEffect } from "react";
+import { baseUrl } from "../constants/movie";
+import { Movie } from "../typings";
 
 interface Props {
-  netflixOriginals: Movie[]
+  netflixOriginals: Movie[];
 }
 
 function Banner({ netflixOriginals }: Props) {
-
-  const [movie, setMovie] = useState<Movie | null>(null)
+  const [movie, setMovie] = useState<Movie | null>(null);
 
   useEffect(() => {
     setMovie(
       netflixOriginals[Math.floor(Math.random() * netflixOriginals.length)]
-    )
-  }, [netflixOriginals])
+    );
+  }, [netflixOriginals]);
+
+  console.log(movie);
 
   return (
     <div>
-        <div>
-            <Image src={`${baseUrl}${movie?.backdrop_path || movie?.poster_path}`}/>
-        </div>
+      <div className="absolute top-0 left-0 -z-10 h-[95vh] w-screen">
+        <Image
+          src={`${baseUrl}${movie?.backdrop_path || movie?.poster_path}`}
+          layout="fill"
+          objectFit='cover'
+        />
+      </div>
+
+      <h1></h1>
     </div>
-  )
+  );
 }
 
 export default Banner
