2de354f3bed08438ff095b475dfe522126cf9f34
qxan <mehmet.enfa@gmail.com>
2023-02-11 16:20:42 +0300

    second update

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -3,7 +3,7 @@ import { useState, useEffect } from "react";
 import { baseUrl } from "../constants/movie";
 import { Movie } from "../typings";
 import { FaPlay } from "react-icons/fa";
-import { InformationCircleIcon } from '@heroicons/react/outline'
+import { GrCircleInformation } from "react-icons/gr";
 
 interface Props {
   netflixOriginals: Movie[];
@@ -20,24 +20,26 @@ function Banner({ netflixOriginals }: Props) {
     <div className="flex flex-col space-y-2 py-16 md:space-y-4 lg:h-[65vh] lg:justify-end lg:pb-12">
       <div className="absolute top-0 left-0 -z-10 h-[95vh] w-screen">
         <Image
-          src={`${baseUrl}${movie?.backdrop_path || movie?.poster_path}`}
           layout="fill"
+          src={`${baseUrl}${movie?.backdrop_path || movie?.poster_path}`}
           objectFit="cover"
         />
       </div>
-      <h1 className="text-2xl lg:text-7xl font-bold md:text-4xl">
+
+      <h1 className="text-2xl font-bold md:text-4xl lg:text-7xl">
         {movie?.title || movie?.name || movie?.original_name}
       </h1>
-      <p className="mx-w-xs text-xs md:max-w-lg md:text-lg lg:max-w-2xl lg:text-2xl">
+      <p className="max-w-xs text-xs text-shadow-md md:max-w-lg md:text-lg lg:max-w-2xl lg:text-2xl">
         {movie?.overview}
       </p>
       <div className="flex space-x-3">
         <button className="bannerButton bg-white text-black">
-          <FaPlay className="h-4 w-4 text-black md:h-7 md:w-7" /> Play
+          <FaPlay className="h-4 w-4 text-black md:h-7 md:w-7" />
+          Play
         </button>
         <button className="bannerButton bg-[gray]/70 ">
+          <GrCircleInformation className="w-5 h-5 md:h-8 md:w-8" />
           More Info
-          <InformationCircleIcon className="w-5 h-5 md:h-8 md:w-8" />
         </button>
       </div>
     </div>
